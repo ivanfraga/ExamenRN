a642ee4b6056b1908dbe41f17bb5be69c00d4d3b
ivan fraga <edwin.fraga@epn.edu.ec>
2022-11-16 08:14:41 -0500

    examen clear minds

---
+++ app/utils/FirebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQz9RCucGAlftiJWB0ztdsA2Eg_1HaXUw",
+  authDomain: "ivanreactn.firebaseapp.com",
+  projectId: "ivanreactn",
+  storageBucket: "ivanreactn.appspot.com",
+  messagingSenderId: "196105707248",
+  appId: "1:196105707248:web:5bd1c86d217e1a24f7ac35"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
